358708eee7b2b2e7137410b8319d39b937a36c83
Yuriy Koziy <yukoziy@gmail.com>
2021-03-12 22:54:05 -0500

    started work on calculator code

--- script/calculator.js
+++ script/calculator.js
@@ -1,1 +1,38 @@
-console.log('calculator.js is working');
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            try {
+                return divide(a, b);
+            } catch (error) {
+                console.log(error);
+                return null;
+            }
+        default:
+            return null;
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (!b) {
+        throw new Error('division by zero');
+    }
+    return a / b;
+}
